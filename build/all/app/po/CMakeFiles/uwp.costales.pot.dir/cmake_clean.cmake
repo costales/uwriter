889dfc678b67a39dd51999105cb2a297aac78b2a
costales <costales.marcos@gmail.com>
2020-09-20 09:46:53 +0200

    Migrated from Oxide to Morph

--- build/all/app/po/CMakeFiles/uwp.costales.pot.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/uwp.costales.pot"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/uwp.costales.pot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
